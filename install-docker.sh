e481867d0cefc6e97e57e9b457ba19bbefcedc9b
Bright Asechemie <61978868+Tamunoseimiebi@users.noreply.github.com>
2022-05-24 20:01:40 +0100

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -17,17 +17,17 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 echo "Adding user to docker group"
 
-sudo usermod -aG docker e4edevops
+sudo usermod -aG docker ubuntu
 
 echo "Installing Docker Compose"
 
-sudo apt-get install docker-compose-plugin
+sudo apt-get install docker-compose-plugin -y
 
 echo "Checking Docker version"
 
